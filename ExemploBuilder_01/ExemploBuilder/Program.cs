c8d60dbc1ab725d39e04ec67aef94a341731effd
Cesar Falanque <felipefalanque@gmail.com>
2020-03-13 15:47:04 -0300

    Estudo GoF Builder

---
+++ ExemploBuilder_01/ExemploBuilder/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ExemploBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Padrão Builder -  Fluent Interface");
+
+            var emailBuilder = new EmailBuilder();
+
+            var email =
+                emailBuilder.To("macoratti@yahoo.com")
+                .From("teste@email.com")
+                .Subject("Assunto do Email")
+                .Body("Teste de envio de email")
+                .Build();
+
+            Console.WriteLine($"{email.From} \n {email.To} \n {email.Subject} \n {email.Body} ");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Email
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class EmailBuilder
+    {
+        private readonly Email _email;
+        public EmailBuilder()
+        {
+            _email = new Email();
+        }
+
+        public EmailBuilder Body(string content)
+        {
+            _email.Body = content;
+            return this;
+        }
+        public EmailBuilder From(string address)
+        {
+            _email.From = address;
+            return this;
+        }
+        public Email Build()
+        {
+            return _email;
+        }
+        public EmailBuilder Subject(string title)
+        {
+            _email.Subject = title;
+            return this;
+        }
+        public EmailBuilder To(string address)
+        {
+            _email.To = address;
+            return this;
+        }
+    }
+}
---
+++ ExemploBuilder_02/ExemploBuilder_02/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace ExemploBuilder_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exercito = new Exercito();
+            CriadorDeGuerreiro criadorDeGuerreiro;
+            Guerreiro guerreiro;
+
+            // criando um guerreiro medieval
+            criadorDeGuerreiro = new CriadorDeGuerreiroMedieval();
+            exercito.ConstruirGuerreiro(criadorDeGuerreiro);
+            guerreiro = criadorDeGuerreiro.ObterGuerreiro();
+            Console.WriteLine("Guerreiro com as características: {0}, {1}, {2}", guerreiro.Arco, guerreiro.Armadura, guerreiro.Espada);
+
+            // criando um guerreiro futurista
+            criadorDeGuerreiro = new CriadorDeGuerreiroFuturista();
+            exercito.ConstruirGuerreiro(criadorDeGuerreiro);
+            guerreiro = criadorDeGuerreiro.ObterGuerreiro();
+            Console.WriteLine("Guerreiro com as características: {0}, {1}, {2}", guerreiro.Arco, guerreiro.Armadura, guerreiro.Espada);
+
+            Console.ReadLine();
+        }
+    }
+
+    // Builder abstrato - fornece uma interface para criação de um guerreiro
+    public abstract class CriadorDeGuerreiro
+    {
+        protected Guerreiro _guerreiro;
+        public Guerreiro ObterGuerreiro()
+        {
+            return _guerreiro;
+        }
+
+        public abstract CriadorDeGuerreiro ComEspada();
+        public abstract CriadorDeGuerreiro ComArmadura();
+        public abstract CriadorDeGuerreiro ComArco();
+    }
+
+    // Produto abstrato - resultado do builder
+    public abstract class Guerreiro
+    {
+        public string Espada { get; protected set; }
+        public string Armadura { get; protected set; }
+        public string Arco { get; protected set; }
+
+        public abstract void EscolherEspada(string espada);
+        public abstract void ColocarArmadura(string armadura);
+        public abstract void EscolherArco(string arco);
+    }
+
+    // Director - faz uso de um builder para construir o produto
+    public class Exercito
+    {
+        public void ConstruirGuerreiro(CriadorDeGuerreiro criadorDeGuerreiro)
+        {
+            //criadorDeGuerreiro.ComArco();
+            //criadorDeGuerreiro.ComArmadura();
+            //criadorDeGuerreiro.ComEspada();
+
+            // montagem do produto de forma fluente
+            criadorDeGuerreiro.ComArco().ComArmadura().ComEspada();
+        }
+    }
+
+    public class CriadorDeGuerreiroMedieval : CriadorDeGuerreiro
+    {
+        public CriadorDeGuerreiroMedieval()
+        {
+            _guerreiro = new GuerreiroMedieval();
+        }
+
+        public override CriadorDeGuerreiro ComEspada()
+        {
+            _guerreiro.EscolherEspada("espada medieval");
+            return this;
+        }
+
+        public override CriadorDeGuerreiro ComArmadura()
+        {
+            _guerreiro.ColocarArmadura("armadura medieval");
+            return this;
+        }
+
+        public override CriadorDeGuerreiro ComArco()
+        {
+            _guerreiro.EscolherArco("arco medieval");
+            return this;
+        }
+    }
+
+    public class GuerreiroMedieval : Guerreiro
+    {
+        public override void EscolherEspada(string espada)
+        {
+            Espada = espada;
+        }
+
+        public override void ColocarArmadura(string armadura)
+        {
+            Armadura = armadura;
+        }
+
+        public override void EscolherArco(string arco)
+        {
+            Arco = arco;
+        }
+    }
+
+    public class CriadorDeGuerreiroFuturista : CriadorDeGuerreiro
+    {
+        public CriadorDeGuerreiroFuturista()
+        {
+            _guerreiro = new GuerreiroFuturista();
+        }
+
+        public override CriadorDeGuerreiro ComEspada()
+        {
+            _guerreiro.EscolherEspada("espada futurista");
+            return this;
+        }
+
+        public override CriadorDeGuerreiro ComArmadura()
+        {
+            _guerreiro.ColocarArmadura("armadura futurista");
+            return this;
+        }
+
+        public override CriadorDeGuerreiro ComArco()
+        {
+            _guerreiro.EscolherArco("arco futurista");
+            return this;
+        }
+    }
+
+    public class GuerreiroFuturista : Guerreiro
+    {
+        public override void EscolherEspada(string espada)
+        {
+            Espada = espada;
+        }
+
+        public override void ColocarArmadura(string armadura)
+        {
+            Armadura = armadura;
+        }
+
+        public override void EscolherArco(string arco)
+        {
+            Arco = arco;
+        }
+    }
+}
